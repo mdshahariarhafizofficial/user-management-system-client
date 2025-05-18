669365abb848c11930e8689913399cea9f0e8607
mdshahariarhafizofficial <shahariarwebdev@gmail.com>
2025-05-18 20:30:54 +0600

    basic setup done

---
+++ src/Routes/Routes.jsx
@@ -0,0 +1,9 @@
+import { createBrowserRouter } from "react-router";
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <h2>Hlw</h2>
+    }
+])
+export default router;
